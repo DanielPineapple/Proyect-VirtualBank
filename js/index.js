aeb658662336b5add891a6d0f1be2412bd31aaaf
Pineapple <danieltrabajo1618@gmail.com>
2024-02-08 17:51:10 -0300

    Hago primera parte del home

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const home = document.getElementById("homeid");
+var hero = document.getElementById("heropub");
+const loguIn = document.getElementById("loguin");
+
+loguIn.innerHTML+= `  <div>
+                    <h1>Regístrate</h1>
+                    <input type="text" name="usuario" value="" placeholder="Usuario"></input>
+                    <input type="password" name="contraseña" value="" placeholder="Contraseña"></input>
+                    <input type="submit" name="" value="Ingresar"></input>
+                    </div>`

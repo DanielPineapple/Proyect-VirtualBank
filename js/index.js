8d1a5ca730a68d274840dd5557b6ac2ceed057d5
Pineapple <danieltrabajo1618@gmail.com>
2024-02-18 22:54:37 -0300

    Doy estilos a index home

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ var hero = document.getElementById("heropub");
 const loguIn = document.getElementById("loguin");
 
 loguIn.innerHTML+= `  <div>
-                    <h1>Regístrate</h1>
-                    <input type="text" name="usuario" value="" placeholder="Usuario"></input>
-                    <input type="password" name="contraseña" value="" placeholder="Contraseña"></input>
-                    <input type="submit" name="" value="Ingresar"></input>
+                    <h1 class="loguin__text">Regístrate</h1>
+                    <input type="text" name="usuario" value="" placeholder="Usuario" class="intro__usu"></input>
+                    <input type="password" name="contraseña" value="" placeholder="Contraseña" class="intro__pass"></input>
+                    <input type="submit" name="" value="Ingresar" class="button__text"></input>
                     </div>`

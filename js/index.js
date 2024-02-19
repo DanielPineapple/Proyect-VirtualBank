dedebeaf38f292948f0b2fe6e40d8335e0107ea8
Pineapple <danieltrabajo1618@gmail.com>
2024-02-19 17:49:21 -0300

    Hago resposive

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const home = document.getElementById("homeid");
 var hero = document.getElementById("heropub");
 const loguIn = document.getElementById("loguin");
 
-loguIn.innerHTML+= `  <div>
+loguIn.innerHTML+= `  <div class="loguin__cont">
                     <h1 class="loguin__text">Regístrate</h1>
                     <input type="text" name="usuario" value="" placeholder="Usuario" class="intro__usu"></input>
                     <input type="password" name="contraseña" value="" placeholder="Contraseña" class="intro__pass"></input>
